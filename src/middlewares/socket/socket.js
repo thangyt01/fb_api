f916dbe6eb8d2dbaa2b114020ff025cd333d2492
thangyt01 <thangyt00@gmail.com>
2022-10-23 09:48:32 +0700

    init socket

---
+++ src/middlewares/socket/socket.js
@@ -0,0 +1,38 @@
+const { BksService } = require("../../components/bks/BksService")
+const { authentication } = require("./auth")
+
+module.exports = (bks) => {
+    bks.use(authentication).on('connection', async (socket) => {
+        try {
+            const { loginUser = {} } = socket
+            socket.on('join-room', () => {
+                const params = {
+                    loginUser,
+                    socketId: socket.id,
+                    bks
+                }
+                BksService.joinRoom(params)
+            })
+
+            socket.on('leave-room', () => {
+                const params = {
+                    loginUser,
+                    socketId: socket.id,
+                    bks
+                }
+                BksService.leaveRoom(params)
+            })
+
+            socket.on('server-new-post', (data) => {
+                const params = {
+                    data,
+                    bks,
+                    loginUser
+                }
+                BksService.createPost(params)
+            })
+        } catch (error) {
+            console.log(`socket bks error ${e.stack || JSON.stringify(e)}`)
+        }
+    })
+}
